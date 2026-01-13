e937192b4cf06821ff744aa74790a563933252e3
dwivediprashant <dwivediprashant.0219@gmail.com>
2026-01-13 15:20:44 +0530

    update : removed market API key from market.js

--- backend/routes/market.js
+++ backend/routes/market.js
@@ -5,7 +5,7 @@ const MarketCache = require('../models/MarketCache');
 const router = express.Router();
 
 const GOVT_API_BASE = 'https://api.data.gov.in/resource/9ef84268-d588-465a-a308-a864a43d0070';
-const API_KEY = '579b464db66ec23bdd000001cdd3946e44ce4aad7209ff7b23ac571b';
+const API_KEY = process.env.AGMARKNET_KEY;
 
 // Get market prices
 router.get('/', async (req, res) => {
